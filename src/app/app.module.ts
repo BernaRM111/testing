87128f1902c624d6865a44dd608c05a06418fd3a
Bernardo Rosas <124811292+BernaRM111@users.noreply.github.com>
2023-10-13 14:31:30 -0600

    Stddev (#15)
    * add stddve branch #6
    
    * new update stddev #11

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MediaComponent } from './media/media.component';
+import { StddevComponent } from './stddev/stddev.component';
 
 import { StddevComponent } from './stddev/stddev.component';
 
