eecc6871ab7dd0e85c0c2831fb811301deb9b58f
Bernardo Rosas <124811292+BernaRM111@users.noreply.github.com>
2023-10-13 11:57:46 -0600

    Media (#14)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MediaComponent } from './media/media.component';
+
 import { StddevComponent } from './stddev/stddev.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
+
     MediaComponent,
     StddevComponent
+
   ],
   imports: [
     BrowserModule,
