68d1c8002a03c62bc2cd9eee99c00d64af4c4fbb
BernaRM111 <motoberna222@gmail.com>
2023-10-15 12:17:37 -0600

    new upda stddev #11

--- src/app/stddev/stddev.component.spec.ts
+++ src/app/stddev/stddev.component.spec.ts
@@ -1,38 +1,3 @@
-/*
-
-describe('StddevComponent', () => {
-  let component: StddevComponent;
-  let fixture: ComponentFixture<StddevComponent>;
-  let stddevService: StddevService;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [StddevComponent],
-      providers: [StddevService],
-      imports: [HttpClientModule]
-    }).compileComponents();
-
-    fixture = TestBed.createComponent(StddevComponent);
-    component = fixture.componentInstance;
-    stddevService = TestBed.inject(StddevService);
-  });
-
-  const detectChangesAndStabilize = async () => {
-    fixture.detectChanges();
-    await fixture.whenStable();
-  };
-
-  it('Should return stddev=62.26 with the data: 15.0, 69.9, 6.5, 22.4, 28.4, 65.9, 19.4, 198.7, 38.8, 138.2', async () => {
-    await detectChangesAndStabilize();
-    expect(component.devHoursStdDev).toBeCloseTo(62.26, 2);
-  });
-
-  it('Should return stddev=572.03 with the data: 160, 591, 114, 229, 230, 270, 128, 1657, 624, 1503', async () => {
-    await detectChangesAndStabilize();
-    expect(component.proxySizeStdDev).toBeCloseTo(572.03, 2);
-  });
-});*/
-
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { StddevComponent } from './stddev.component';
 import { StddevService } from '../services/stddev.service';
