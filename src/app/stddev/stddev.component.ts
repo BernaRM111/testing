dca638fb67ceec06b0e510549a5e5d53e340a4bb
Bernardo Rosas <124811292+BernaRM111@users.noreply.github.com>
2023-10-15 12:32:52 -0600

    Stddev (#19)
    * add stddve branch #6
    
    * new update stddev #11
    
    * update stddev #11
    
    * new upda stddev #11
    
    * new stddev #11
    
    * new stddev ac #11

--- src/app/stddev/stddev.component.ts
+++ src/app/stddev/stddev.component.ts
@@ -9,8 +9,6 @@ import { media } from '../media/media.component';
 })
 
 export class StddevComponent implements OnInit {
-  /*devHoursStddev: number = 0;
-  proxySizeStddev: number = 0;*/
   proxySizeStdDev: number = 0;
   devHoursStdDev: number = 0;
 
@@ -21,41 +19,23 @@ export class StddevComponent implements OnInit {
     this.calculateDevHoursStandardDeviation();
   }
 
-  /*ngOnInit(): void {
-    this.subscribeAndCalculateStddev(this.stddevService.getDevHours(), (stddev) => this.devHoursStddev = stddev);
-    this.subscribeAndCalculateStddev(this.stddevService.getProxySize(), (stddev) => this.proxySizeStddev = stddev);
-  }*/
-
-  /*private subscribeAndCalculateStddev(observable: Observable<number[]>, callback: { (stddev: any): any; (stddev: any): any; (arg0: number): void; }): void {
-    observable.subscribe(data => {
-      callback(this.calcularDesviacionEstandar(data));
-    });
-  }*/
-
-  /*private calcularDesviacionEstandar(data: number[]): number {
-    const n = data.length;
-    const mean = data.reduce((acc, val) => acc + val, 0) / n;
-    const sumSquaredDiff = data.reduce((acc, val) => acc + Math.pow(val - mean, 2), 0);
-    return Math.sqrt(sumSquaredDiff / (n - 1));
-  }*/
-
   calculateProxySizeStandardDeviation() {
     this.stddevService.getProxySize().subscribe(data => { 
       const mean = media(data);
-      this.proxySizeStdDev = this.calcularDesviacionEstandar(data, mean);
+      this.proxySizeStdDev = this.desviacionEstandar(data, mean);
     });
   }
 
   calculateDevHoursStandardDeviation() {
     this.stddevService.getDevHours().subscribe(data => { 
       const mean = media(data);
-      this.devHoursStdDev = this.calcularDesviacionEstandar(data, mean);
+      this.devHoursStdDev = this.desviacionEstandar(data, mean);
     });
   }
 
-  calcularDesviacionEstandar(data: number[], mean: number): number {
+  desviacionEstandar(data: number[], mean: number): number {
     const squaredDifferences = data.map(val => Math.pow(val - mean, 2));
-    const meanOfSquaredDifferences = media(squaredDifferences); // Usar la función calcularMedia
+    const meanOfSquaredDifferences = media(squaredDifferences);
     const stdDev = Math.sqrt(meanOfSquaredDifferences);
     return stdDev;
   }
