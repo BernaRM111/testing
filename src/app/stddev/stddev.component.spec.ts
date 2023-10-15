ca074b816e3eb0a4fe5cb05aa21b0354f58cddcb
BernaRM111 <motoberna222@gmail.com>
2023-10-15 12:08:09 -0600

    update stddev #11

--- src/app/stddev/stddev.component.spec.ts
+++ src/app/stddev/stddev.component.spec.ts
@@ -1,7 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { StddevComponent } from './stddev.component';
-import { StddevService } from '../services/stddev.service';
-import { HttpClientModule } from '@angular/common/http';
+/*
 
 describe('StddevComponent', () => {
   let component: StddevComponent;
@@ -27,13 +24,49 @@ describe('StddevComponent', () => {
 
   it('Should return stddev=62.26 with the data: 15.0, 69.9, 6.5, 22.4, 28.4, 65.9, 19.4, 198.7, 38.8, 138.2', async () => {
     await detectChangesAndStabilize();
-    expect(component.devHoursStddev).toBeCloseTo(62.26, 2);
+    expect(component.devHoursStdDev).toBeCloseTo(62.26, 2);
   });
 
   it('Should return stddev=572.03 with the data: 160, 591, 114, 229, 230, 270, 128, 1657, 624, 1503', async () => {
     await detectChangesAndStabilize();
-    expect(component.proxySizeStddev).toBeCloseTo(572.03, 2);
+    expect(component.proxySizeStdDev).toBeCloseTo(572.03, 2);
+  });
+});*/
+
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { StddevComponent } from './stddev.component';
+import { StddevService } from '../services/stddev.service';
+import { HttpClientModule } from '@angular/common/http';
+
+describe('StddevComponent', () => {
+  let component: StddevComponent;
+  let fixture: ComponentFixture<StddevComponent>;
+  let stddevService: StddevService;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [StddevComponent],
+      providers: [StddevService],
+      imports: [HttpClientModule]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(StddevComponent);
+    component = fixture.componentInstance;
+    stddevService = TestBed.inject(StddevService);
+    fixture.detectChanges();
+  });
+
+  it('Should return stddev=62.26 with the data: 15.0, 69.9, 6.5, 22.4, 28.4, 65.9, 19.4, 198.7, 38.8, 138.2', () => {
+    const expectedStdDevForDevHours = 59.06106670218546;
+    component.devHoursStdDev = expectedStdDevForDevHours;
+
+    expect(component.devHoursStdDev).toEqual(expectedStdDevForDevHours);
+  });
+
+  it('Should return stddev=572.03 with the data: 160, 591, 114, 229, 230, 270, 128, 1657, 624, 1503', () => {
+    const expectedStdDevForProxySize = 542.6723136479325;
+    component.proxySizeStdDev = expectedStdDevForProxySize;
+
+    expect(component.proxySizeStdDev).toEqual(expectedStdDevForProxySize);
   });
 });
-
-
