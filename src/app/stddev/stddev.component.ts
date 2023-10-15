6dba78f015c8b80d1687424b1087b027e9fb3860
Bernardo Rosas <124811292+BernaRM111@users.noreply.github.com>
2023-10-15 12:10:55 -0600

    Stddev (#17)
    * add stddve branch #6
    
    * new update stddev #11
    
    * update stddev #11

--- src/app/stddev/stddev.component.ts
+++ src/app/stddev/stddev.component.ts
@@ -1,66 +1,64 @@
-/*import { Component, OnInit } from '@angular/core';
-import { StddevService } from '../services/stddev.service';
-
-@Component({
-  selector: 'app-stddev',
-  template: ''
-})
-export class StddevComponent implements OnInit {
-  devHoursStddev: number = 0;
-  proxySizeStddev: number = 0;
-
-  constructor(private stddevService: StddevService) {}
-
-  ngOnInit(): void {
-    this.stddevService.getDevHours().subscribe(data => {
-      this.devHoursStddev = this.calcularDesviacionEstandar(data);
-    });
-
-    this.stddevService.getProxySize().subscribe(data => {
-      this.proxySizeStddev = this.calcularDesviacionEstandar(data);
-    });
-  }
-
-  private calcularDesviacionEstandar(data: number[]): number {
-    const n = data.length;
-    const mean = data.reduce((acc, val) => acc + val, 0) / n;
-    const sumSquaredDiff = data.reduce((acc, val) => acc + Math.pow(val - mean, 2), 0);
-    return Math.sqrt(sumSquaredDiff / (n - 1));
-  }
-}*/
 import { Component, OnInit } from '@angular/core';
 import { StddevService } from '../services/stddev.service';
 import { Observable } from 'rxjs';
+import { media } from '../media/media.component';
 
 @Component({
   selector: 'app-stddev',
   template: ''
 })
 
-
-
 export class StddevComponent implements OnInit {
-  devHoursStddev: number = 0;
-  proxySizeStddev: number = 0;
+  /*devHoursStddev: number = 0;
+  proxySizeStddev: number = 0;*/
+  proxySizeStdDev: number = 0;
+  devHoursStdDev: number = 0;
 
   constructor(private stddevService: StddevService) {}
 
-  ngOnInit(): void {
-    this.subscribeAndCalculateStddev(this.stddevService.getDevHours(), (stddev) => this.devHoursStddev = stddev);
-    this.subscribeAndCalculateStddev(this.stddevService.getProxySize(), (stddev) => this.proxySizeStddev = stddev);
+  ngOnInit() {
+    this.calculateProxySizeStandardDeviation();
+    this.calculateDevHoursStandardDeviation();
   }
 
-  private subscribeAndCalculateStddev(observable: Observable<number[]>, callback: { (stddev: any): any; (stddev: any): any; (arg0: number): void; }): void {
+  /*ngOnInit(): void {
+    this.subscribeAndCalculateStddev(this.stddevService.getDevHours(), (stddev) => this.devHoursStddev = stddev);
+    this.subscribeAndCalculateStddev(this.stddevService.getProxySize(), (stddev) => this.proxySizeStddev = stddev);
+  }*/
+
+  /*private subscribeAndCalculateStddev(observable: Observable<number[]>, callback: { (stddev: any): any; (stddev: any): any; (arg0: number): void; }): void {
     observable.subscribe(data => {
       callback(this.calcularDesviacionEstandar(data));
     });
-  }
+  }*/
 
-  private calcularDesviacionEstandar(data: number[]): number {
+  /*private calcularDesviacionEstandar(data: number[]): number {
     const n = data.length;
     const mean = data.reduce((acc, val) => acc + val, 0) / n;
     const sumSquaredDiff = data.reduce((acc, val) => acc + Math.pow(val - mean, 2), 0);
     return Math.sqrt(sumSquaredDiff / (n - 1));
+  }*/
+
+  calculateProxySizeStandardDeviation() {
+    this.stddevService.getProxySize().subscribe(data => { 
+      const mean = media(data);
+      this.proxySizeStdDev = this.calcularDesviacionEstandar(data, mean);
+    });
   }
+
+  calculateDevHoursStandardDeviation() {
+    this.stddevService.getDevHours().subscribe(data => { 
+      const mean = media(data);
+      this.devHoursStdDev = this.calcularDesviacionEstandar(data, mean);
+    });
+  }
+
+  calcularDesviacionEstandar(data: number[], mean: number): number {
+    const squaredDifferences = data.map(val => Math.pow(val - mean, 2));
+    const meanOfSquaredDifferences = media(squaredDifferences); // Usar la función calcularMedia
+    const stdDev = Math.sqrt(meanOfSquaredDifferences);
+    return stdDev;
+  }
+
 }
 
