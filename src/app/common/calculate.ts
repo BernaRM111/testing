ad8f52a6f2530cc4bda5cdf3a29a825bad862d0c
BernaRM111 <motoberna222@gmail.com>
2023-10-23 08:46:40 -0600

    update

--- src/app/common/calculate.ts
+++ src/app/common/calculate.ts
@@ -14,6 +14,10 @@ export function sumaX2(valoresX: number[]): number {
   return valoresX.reduce((acc, x) => acc + Math.pow(x, 2), 0);
 }
 
+export function sumaY2(dataY: number[]): number {
+  return dataY.reduce((acc, y) => acc + Math.pow(y, 2), 0);
+}
+
 export function media(datos: number[]) {
   console.log(datos)
   if (datos.length === 0) {
