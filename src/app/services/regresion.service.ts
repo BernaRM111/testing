40056f1cf083ca4bb309e19e25ccac80bd512c66
BernaRM111 <motoberna222@gmail.com>
2023-10-23 11:03:14 -0600

    update regression #22

--- src/app/services/regresion.service.ts
+++ src/app/services/regresion.service.ts
@@ -1,5 +1,5 @@
-/*import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
@@ -11,32 +11,32 @@ export class RegresionService {
 
   constructor(private http: HttpClient) {}
 
-  getTest1(): Observable<number[]> {
-    return this.http.get<{data: number[]}>(`${this.baseURL}/3a_test1`).pipe(
-      map(response => response.data)
-    );
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
   }
 
-  getTest2(): Observable<number[]> {
-    return this.http.get<{data: number[]}>(`${this.baseURL}/3a_test2`).pipe(
-      map(response => response.data)
-    );
+  getTest1(): Observable<any> {
+    return this.http.get<any>(`${this.baseURL}/3a_test1`,this.httpOptions)
   }
 
-  getTest3(): Observable<number[]> {
-    return this.http.get<{data: number[]}>(`${this.baseURL}/3a_test3`).pipe(
-      map(response => response.data)
-    );
+  getTest2(): Observable<any> {
+    return this.http.get<any>(`${this.baseURL}/3a_test2`,this.httpOptions)
+    
   }
 
-  getTest4(): Observable<number[]> {
-    return this.http.get<{data: number[]}>(`${this.baseURL}/3a_test4`).pipe(
-      map(response => response.data)
-    );
+  getTest3(): Observable<any> {
+    return this.http.get<any>(`${this.baseURL}/3a_test3`,this.httpOptions)
   }
 
-}*/
+  getTest4(): Observable<any> {
+    return this.http.get<any>(`${this.baseURL}/3a_test4`, this.httpOptions)
+  }
 
+}
+
+/*
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, catchError, retry, throwError } from 'rxjs';
@@ -47,9 +47,7 @@ import { Observable, catchError, retry, throwError } from 'rxjs';
 export class RegresionService {
 
 
-  constructor(
-    private readonly http: HttpClient
-  ) { }
+  constructor(private readonly http: HttpClient) { }
 
   test1 = 'https://api-testing-service-bernarm111.cloud.okteto.net/3a_test1'
   test2 = 'https://api-testing-service-bernarm111.cloud.okteto.net/3a_test2'
@@ -63,31 +61,35 @@ export class RegresionService {
   }
 
   getTest1(): Observable<any> {
-    return this.http.get<any>(this.test1, this.httpOptions).pipe(
+    /*return this.http.get<any>(this.test1, this.httpOptions).pipe(
       retry(1),
       catchError(this.handleError)
-    )
+    )*/
+    /*return this.http.get<any>(this.test1, this.httpOptions)
   }
   getTest2(): Observable<any> {
-    return this.http.get<any>(this.test2, this.httpOptions).pipe(
+    /*return this.http.get<any>(this.test2, this.httpOptions).pipe(
       retry(1),
       catchError(this.handleError)
-    )
+    )*/
+    /*return this.http.get<any>(this.test2, this.httpOptions)
   }
   getTest3(): Observable<any> {
-    return this.http.get<any>(this.test3, this.httpOptions).pipe(
+    /*return this.http.get<any>(this.test3, this.httpOptions).pipe(
       retry(1),
       catchError(this.handleError)
-    )
+    )*/
+    /*return this.http.get<any>(this.test3, this.httpOptions)
   }
   getTest4(): Observable<any> {
-    return this.http.get<any>(this.test4, this.httpOptions).pipe(
+    /*return this.http.get<any>(this.test4, this.httpOptions).pipe(
       retry(1),
       catchError(this.handleError)
-    )
+    )*/
+    /*return this.http.get<any>(this.test4, this.httpOptions)
   }
 
-  handleError(error: any) {
+  /*handleError(error: any) {
     let errorMessage = ''
     if (error.error instanceof ErrorEvent) {
       errorMessage = error.error.message;
@@ -96,5 +98,6 @@ export class RegresionService {
     }
     window.alert(errorMessage)
     return throwError(errorMessage)
-  }
+  }*//*
 }
+*/
