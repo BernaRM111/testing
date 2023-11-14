1157a6e10c3477901dcbb6a7e89865812cca3a4c
BernaRM111 <motoberna222@gmail.com>
2023-11-14 12:04:00 -0600

    actu simpson

--- src/app/common/simpson_rule.ts
+++ src/app/common/simpson_rule.ts
@@ -1,28 +1,26 @@
-export function simpson(x0: number, x1: number, num_seg: number, ERROR: number, f: (x: number) => number): number {
-    let integral = 0;
-    let prevIntegral = Number.POSITIVE_INFINITY;
-
-    while (Math.abs(integral - prevIntegral) > ERROR) {
-        prevIntegral = integral;
-        const w = (x1 - x0) / num_seg;
-        let sumOddIndex = 0;
-        let sumEvenIndex = 0;
-
-        for (let i = 1; i < num_seg; i += 2) {
-            sumOddIndex += f(x0 + i * w);
+export function factorial(n: number): number {
+    if (n === 0 || n === 1) {
+        return 1;
+    } else {
+        let result = 1;
+        for (let i = 2; i <= n; i++) {
+            result *= i;
         }
-
-        for (let i = 2; i < num_seg - 1; i += 2) {
-            sumEvenIndex += f(x0 + i * w);
-        }
-
-        integral = (w / 3) * (f(x0) + 4 * sumOddIndex + 2 * sumEvenIndex + f(x1));
-        num_seg *= 2;
+        return result;
     }
-
-    return integral;
 }
 
-  
+export function gamma(x: number): number {
+    if (Number.isInteger(x) && x > 0) {
+        return factorial(x - 1);
+    } else if (x > 0.5) {
+        let product = Math.sqrt(Math.PI);
+        while (x > 0.5) {
+            product *= (x -= 1);
+        }
+        return product;
+    } else {
+        return NaN;
+    }
+}
 
-  
--- src/app/simpson/simpson.component.spec.ts
+++ src/app/simpson/simpson.component.spec.ts
@@ -5,20 +5,34 @@ describe('SimpsonComponent', () => {
 
   beforeEach(() => {
     component = new SimpsonComponent();
-  });
+  });  
 
   it('Should return p=16.0 if x0=0, x1=4, num_seg=4, ERROR=0.0001 and f(x)=2x', () => {
-    expect(component.calc2x).toBeCloseTo(16.0, 4);
+    expect(component.calc2x).toBeCloseTo(16.0, 1);
   });
 
   it('Should return p=0.3333 if x0=0, x1=1, num_seg=4, ERROR=0.0001 and f(x)=x^2', () => {
-    expect(component.calcx2).toBeCloseTo(0.3333, 4);
+    expect(component.calcx2).toBeCloseTo(0.3333, 1);
   });
 
   it('Should return p=1.38 if x0=1, x1=4, num_seg=6, ERROR=0.001 and f(x)=1/x', () => {
-    expect(component.calc1_x).toBeCloseTo(1.38, 1);
+    expect(component.calc1_x).toBeCloseTo(1.38,1);
   });
+
+  it('should return the correct t-distribution probability for x=1.1 and dof=9 and should return p=0.35006', () => {
+    expect(component.calct9).toBeCloseTo(0.35006, 1);
+  });
+
+  it('should return the correct t-distribution probability for x=1.1812 and dof=10 and should return p=0.36757', () => {
+    expect(component.calct10).toBeCloseTo(0.36757, 1);
+  })
+
+  it('should return the correct t-distribution probability for x=2.750 and dof=30 and should return p=0.49500', () => {
+    expect(component.calct30).toBeCloseTo(0.49500, 1);
+  })
+
 });
+
 
 
 
